e3eebbc1b5f498d1236078cfa39b371d7bd4f975
ralfy <ralfy@RALFY>
2025-03-11 01:13:09 -0300

    modelo de encuestas

---
+++ Models/Encuestas.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ralfy_Genao_P2_AP1.Models
+{
+    public class Encuestas
+    {
+        [Key]
+        public int EncuestasId { get; set; }
+
+        [Required(ErrorMessage = "La fecha es obligatoria")]
+        public DateTime Fecha { get; set; } = DateTime.Now;
+
+        [Required(ErrorMessage = "La asignatura es obligatoria")]
+        public string? Asignatura { get; set; }
+
+        public virtual ICollection<EncuestaDetalles> EncuestaDetalles { get; set; } = new List<EncuestaDetalles>();
+    }
+}
