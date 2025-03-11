939da1f592af6c82138c0e795879fe3e4366fa31
ralfy <ralfy@RALFY>
2025-03-11 01:12:57 -0300

    Modelo de encuestadetalles

---
+++ Models/EncuestaDetalles.cs
@@ -0,0 +1,17 @@
+﻿using static Azure.Core.HttpHeader;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ralfy_Genao_P2_AP1.Models
+{
+    public class EncuestaDetalles
+    {
+        [Key]
+        public int DetallesId { get; set; }
+        public int EncuestaId { get; set; } 
+        public int CiudadId { get; set; }
+        public int MontoEncuesta { get; set; }
+        [ForeignKey("CiudadId")]
+        public virtual Ciudades Ciudad{ get; set; }
+    }
+}
