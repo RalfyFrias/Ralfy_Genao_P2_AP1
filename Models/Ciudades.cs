d57c0960a01b8b776ad3cf741c2f6ae33fbbb6e4
ralfy <ralfy@RALFY>
2025-03-10 21:58:00 -0300

    creando los modelos

---
+++ Models/Ciudades.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ralfy_Genao_P2_AP1.Models
+{
+    public class Ciudades 
+    {
+        [Key]
+        public int CiudadId { get; set; }
+        public string Nombre { get; set; }
+        public DateTime Fecha { get; set; }
+        public decimal Monto {  get; set; }
+        public string Asignatura { get; set; }
+
+
+    }
+}
