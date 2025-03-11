ba721beea52c1906e065cd634e83054303cd4373
ralfy <ralfy@RALFY>
2025-03-11 01:12:28 -0300

    contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -3,19 +3,22 @@ using Ralfy_Genao_P2_AP1.Models;
 
 namespace Ralfy_Genao_P2_AP1.DAL
 {
-    public class Contexto : DbContext
+    public class Context : DbContext
     {
-        public Contexto(DbContextOptions<Contexto> options)
-              : base(options) { }
-        public DbSet<Ciudades> ciudades { get; set; }
+        public Context(DbContextOptions<Context> options) : base(options) { }
+
+        public DbSet<Ciudades> Ciudades { get; set; }
+        public DbSet<Encuestas> Encuestas { get; set; }
+        public DbSet<EncuestaDetalles> EncuestaDetalles { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Ciudades>().HasData(
-                new Ciudades { CiudadId = 1, Nombre = "Santo Domingo", Monto = 25000 },
-                new Ciudades { CiudadId = 2, Nombre = "Rio San Juan", Monto = 25000 },
-                new Ciudades { CiudadId = 3, Nombre = "Puerto Plata ", Monto = 15500 }
+                new Ciudades { CiudadesId = 1, Nombre = "Santo Domingo", Monto = 25000 },
+                new Ciudades { CiudadesId = 2, Nombre = "Rio San Juan", Monto = 25000 },
+                new Ciudades { CiudadesId = 3, Nombre = "Puerto Plata", Monto = 15500 }
             );
-
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
